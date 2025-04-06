0ebdf5a88af5e88f389421a0fbd24524f1854478
hossainemruz <hossainemruz@gmail.com>
2025-04-06 23:12:44 +0600

    Update tailwind version to 4.x
    Signed-off-by: hossainemruz <hossainemruz@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.md'],
-  theme: {
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-        "half-screen": "50vh",
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            "code::before": { content: '' },
-            "code::after": { content: '' }
-          }
-        }
-      }
-    }
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+/*
+This file is present to satisfy a requirement of the Tailwind CSS IntelliSense
+extension for Visual Studio Code.
+
+https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+
+*/
